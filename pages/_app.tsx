09d665ec784ae4fef65494af123f239e174c15b4
darko.tomicic <dtomicic192@gmail.com>
2023-07-04 21:21:24 +0200

    finished setting fonts up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
-import type { AppProps } from 'next/app'
-import GlobalStyle from '../styles/globalstyles'
+import type { AppProps } from "next/app";
+import GlobalStyle from "../styles/globalstyles";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-        <GlobalStyle />
-        <Component {...pageProps} />
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+                <title>devlinks</title>
+            </Head>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
 }
